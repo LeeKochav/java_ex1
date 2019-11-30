a2b9f5064aee68b01753a869abb2f59352eb3949
Lee Kochav <leekochav@gmail.com>
2019-11-30 16:50:49 +0200

    Third draft

---
+++ Ex1Testing/Ex1/ComplexFunctionTest.java
@@ -0,0 +1,64 @@
+package Ex1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplexFunctionTest {
+
+    @Test
+    void plus() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void max() {
+    }
+
+    @Test
+    void min() {
+    }
+
+    @Test
+    void comp() {
+    }
+
+    @Test
+    void left() {
+    }
+
+    @Test
+    void right() {
+    }
+
+    @Test
+    void getOp() {
+    }
+
+    @Test
+    void f() {
+    }
+
+    @Test
+    void initFromString() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
